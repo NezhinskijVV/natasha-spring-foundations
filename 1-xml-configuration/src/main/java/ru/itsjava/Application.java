bccc7954e57bf26905e576a20d1e832e6ddd5575
Vitaliy <SeekerOfConfessor@gmail.com>
2021-02-19 15:16:18 +0300

    Initial commit

---
+++ 1-xml-configuration/src/main/java/ru/itsjava/Application.java
@@ -0,0 +1,16 @@
+package ru.itsjava;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.itsjava.services.NotebookServiceImpl;
+import ru.itsjava.services.ProgrammerService;
+
+public class Application {
+
+    public static void main(String[] args) {
+        var context = new ClassPathXmlApplicationContext("context.xml");
+
+        context.getBean(ProgrammerService.class).printEquipment();
+
+    }
+}
